1d776f557541fa56bcb0a02987ac371b91363e82
kazouki <kazouki@hotmail.com>
2020-09-26 15:26:40 +0200

    init ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,9 +2,12 @@ import express from "express";
 import { json } from "body-parser";
 
 const app = express();
-const PORT = 3000;
 app.use(json());
 
-app.listen(PORT, () => {
-  console.log("Listening on port", PORT);
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("test");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
 });
